e5d96a3fc04e50e2a91c7a6bae79cb214e0a0091
TakuroSugahara <k5690033@gmail.com>
2020-03-22 18:36:25 +0900

    chore: first commit

---
+++ layouts/default.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer clipped fixed app class="primary">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            App Name
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            {{ item.title }}
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-content class="grey lighten-4">
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'テンプレートの作成',
+          to: '/templates'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'ISSUEの作成',
+          to: '/issues'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
