9bb83166f6f35cadc67e56368f238e7876ff65bd
TakuroSugahara <k5690033@gmail.com>
2020-03-26 01:42:14 +0900

    style: デフォルトレイアウト作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,74 +1,84 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer clipped fixed app class="primary">
-      <v-list-item>
+    <v-navigation-drawer clipped fixed app class="primary pa-3">
+      <v-list-item class="mt-5">
+        <v-list-item-action class="mr-3">
+          <v-img src="/github-icon.png" width="20" />
+        </v-list-item-action>
         <v-list-item-content>
-          <v-list-item-title class="title">
-            App Name
+          <v-list-item-title class="title font-weight-bold white--text">
+            Github issue
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            {{ item.title }}
+      <v-list class="mt-5">
+        <v-list-item class="mb-4">
+          <v-list-item-content class="body-2 font-weight-bold white--text">
+            <v-list-item-title v-text="organaization"></v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-action class="my-0">
+            <v-btn icon>
+              <v-icon color="#929BAB" size="20">
+                mdi-plus-circle
+              </v-icon>
+            </v-btn>
           </v-list-item-action>
+        </v-list-item>
+        <v-list-item
+          v-for="(template, i) in templates"
+          :key="i"
+          :to="`/templates/${template.id}`"
+        >
+          <v-list-item-action class="mr-3">
+            <v-icon color="white" size="20">mdi-file-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-content class="white--text font-weight-bold my-0">
+            <v-list-item-title v-text="template.title"></v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-content class="grey lighten-4">
-      <v-container>
-        <nuxt />
+    <v-content class="primary">
+      <v-container class="pa-0 mt-0 mr-0">
+        <v-card id="content_base_card" height="100vh" class="pa-5">
+          <div>
+            <nuxt />
+          </div>
+        </v-card>
       </v-container>
     </v-content>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'テンプレート',
-          to: '/templates'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'ISSUE',
-          to: '/issues'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import { Template } from '../models/template'
+import { TemplateRepository } from '../repositories/templateRepository'
+
+@Component({})
+export default class Default extends Vue {
+  organaization: string = 'tetsu-tech'
+  loading: boolean = false
+  templates: Template[] = []
+  templateRepository: TemplateRepository = new TemplateRepository()
+
+  async created() {
+    this.loading = true
+    this.templates = await this.templateRepository.find()
+    this.loading = false
   }
 }
 </script>
+
+<style lang="scss" scoped>
+// vuetify container調整
+.container {
+  max-width: 100vw;
+  padding: 0px;
+}
+#content_base_card {
+  border-top-left-radius: 16px;
+  border-bottom-left-radius: 16px;
+  overflow: hidden;
+}
+</style>
