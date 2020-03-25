d31d2c32faf3a1d7e69fe9505a97edfde9a99e64
TakuroSugahara <k5690033@gmail.com>
2020-03-26 01:42:51 +0900

    style: テンプレートの詳細ページ

--- pages/templates/index.vue
+++ pages/templates/index.vue
@@ -1,36 +1,40 @@
 <template>
-  <div>
-    <h1 class="title mt-5">
-      テンプレートの一覧
-    </h1>
-    <v-btn
-      class="mt-3"
-      color="primary"
-      width="160"
-      height="40"
-      to="/templates/add"
-      >追加</v-btn
-    >
-    <v-container
-      v-if="loading"
-      fill-height
-      align-center
-      justify-center
-      ma-0
-      class="mx-auto"
-      style="height: 260px"
-    >
-      <v-progress-circular indeterminate color="black" width="1" size="50" />
-    </v-container>
-    <v-card v-if="!loading" class="mt-5">
-      <v-data-table
-        :headers="headers"
-        :items="templates"
-        hide-default-header
-        hide-default-footer
-      />
-    </v-card>
-  </div>
+  <v-card flat>
+    <v-card-title>
+      <h1 class="title">
+        テンプレートの一覧
+      </h1>
+    </v-card-title>
+    <v-card-text>
+      <v-btn
+        class="mt-3"
+        color="primary"
+        width="160"
+        height="40"
+        to="/templates/add"
+        >追加</v-btn
+      >
+      <v-container
+        v-if="loading"
+        fill-height
+        align-center
+        justify-center
+        ma-0
+        class="mx-auto"
+        style="height: 260px"
+      >
+        <v-progress-circular indeterminate color="black" width="1" size="50" />
+      </v-container>
+      <v-card v-if="!loading" class="mt-5">
+        <v-data-table
+          :headers="headers"
+          :items="templates"
+          hide-default-header
+          hide-default-footer
+        />
+      </v-card>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script lang="ts">
