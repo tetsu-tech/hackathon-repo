85084e1fbace65fe1568cd0bb308d22ee2cd486d
TakuroSugahara <k5690033@gmail.com>
2020-03-29 01:45:53 +0900

    feat: issueに付けるデータ追加

--- models/issue.ts
+++ models/issue.ts
@@ -1,10 +1,30 @@
 import { BaseEntity } from './baseEntity'
+import { GithubUser } from './githubUser'
+import { Label } from './label'
 
 export class Issue extends BaseEntity<Issue> {
   title: string = ''
   body: string = ''
+  state: 'open' | 'closed' = 'open'
+  assigns: GithubUser[] = []
+  labels: Label[] = []
 
   get canCreate(): boolean {
     return Boolean(this.title && this.body)
+  }
+
+  get existAssign(): boolean {
+    return Boolean(this.assigns.length)
+  }
+
+  get isOpen(): boolean {
+    return this.state === 'open'
+  }
+
+  get firstAssign(): GithubUser {
+    if (!this.existAssign) {
+      return new GithubUser()
+    }
+    return this.assigns[0]
   }
 }
