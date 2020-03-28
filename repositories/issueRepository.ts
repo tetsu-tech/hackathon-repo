a17ab3c1b5ec806720c8204fda718f280f2a8c31
TakuroSugahara <k5690033@gmail.com>
2020-03-29 01:45:04 +0900

    feat: issueRepository

--- repositories/issueRepository.ts
+++ repositories/issueRepository.ts
@@ -1,12 +1,30 @@
+import { GithubUserRepository } from './githubUserRepository'
+import { LabelRepository } from './labelRepository'
 import { Issue } from '@/models/issue'
 import { api } from '@/plugins/api'
 
 export class IssueRepository {
+  githubUserRepository: GithubUserRepository
+  labelRepository: LabelRepository
+
+  constructor() {
+    this.githubUserRepository = new GithubUserRepository()
+    this.labelRepository = new LabelRepository()
+  }
+
   createIssueInstance(data: any): Issue {
     const issue = new Issue()
-    issue.id = data._id
+    issue.id = data.id
     issue.title = data.title
     issue.body = data.body
+    issue.state = data.state
+    issue.assigns = data.assignees.map((a: any) =>
+      this.githubUserRepository.createIssueInstance(a)
+    )
+    issue.labels = data.labels.map((l: any) =>
+      this.labelRepository.createIssueInstance(l)
+    )
+    issue.createdAt = new Date(data.created_at)
     return issue
   }
 
@@ -19,6 +37,7 @@ export class IssueRepository {
 
   async find(): Promise<Issue[]> {
     const { data } = await api.get('/issues')
+    console.log(data)
     return data.map((d: any) => this.createIssueInstance(d))
   }
 
